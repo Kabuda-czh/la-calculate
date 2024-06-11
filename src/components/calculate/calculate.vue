8db157769c897eb7451b27ba86fb3e6e3c4242c0
Kabuda-czh <634469564@qq.com>
2024-06-11 22:49:43 +0800

    ✨ feat: add n-spin & loading

--- src/components/calculate/calculate.vue
+++ src/components/calculate/calculate.vue
@@ -2,6 +2,7 @@
 import type { FormInst, StepsProps } from 'naive-ui'
 import { MdArrowRoundBack, MdArrowRoundForward } from '@vicons/ionicons4'
 
+const loading = ref<boolean>(false)
 const current = ref<number | null>(1)
 const currentStatus = ref<StepsProps['status']>('process')
 
@@ -36,49 +37,25 @@ const addItem = () => {
 
 <template>
   <n-space vertical>
-    <n-steps vertical :current="(current as number)" :status="currentStatus">
-      <n-step title="选择想要搭配的刻印">
-        <ChooseBuild v-show="current === 1" />
-      </n-step>
-      <n-step title="选择能力石头">
-        <AbilityStone v-show="current === 2" />
-      </n-step>
-      <n-step title="Break" />
-      <n-step title="Come Together" description="Here come old flat top He come grooving up slowly" />
-      <n-step title="Something" description="Something in the way she moves Attracts me like no other lover" />
-    </n-steps>
-    <n-space>
-      <n-button-group>
-        <n-button @click="prev">
-          <template #icon>
-            <n-icon>
-              <md-arrow-round-back />
-            </n-icon>
-          </template>
-        </n-button>
-        <n-button @click="next">
-          <template #icon>
-            <n-icon>
-              <md-arrow-round-forward />
-            </n-icon>
-          </template>
-        </n-button>
-      </n-button-group>
-      <n-radio-group v-model:value="currentStatus" size="medium" name="vertical">
-        <n-radio-button value="error">
-          Error
-        </n-radio-button>
-        <n-radio-button value="process">
-          Process
-        </n-radio-button>
-        <n-radio-button value="wait">
-          Wait
-        </n-radio-button>
-        <n-radio-button value="finish">
-          Finish
-        </n-radio-button>
-      </n-radio-group>
-    </n-space>
+    <n-spin :show="loading">
+      <n-steps vertical :current="(current as number)" :status="currentStatus">
+        <n-step title="选择想要搭配的刻印">
+          <ChooseBuild v-show="current === 1" @build-success="buildSetting" />
+        </n-step>
+        <n-step title="选择能力石">
+          <AbilityStone v-show="current === 2" @stone-build-success="stoneSetting" @go-to-prev="prev" />
+        </n-step>
+        <n-step title="选择自身刻印">
+          <ChooseSelfBuild v-show="current === 3" @self-build-success="selfBuildSetting" @go-to-prev="prev" />
+        </n-step>
+        <n-step title="配置首饰金额">
+          <div v-show="current === 4">4</div>
+        </n-step>
+        <n-step title="结算">
+          <div v-show="current === 5">5</div>
+        </n-step>
+      </n-steps>
+    </n-spin>
   </n-space>
 </template>
 
