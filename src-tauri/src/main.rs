52b5ce69a2899dd91a7b8446561934c80989c27c
Kabuda-czh <634469564@qq.com>
2024-06-14 17:19:28 +0800

    ✨ feat: use rust algo to calculate data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,17 +3,30 @@
 
 mod calculate;
 
-use calculate::BuildParam;
+use calculate::type_struct::{
+    CalculatePageParam, CalculatePriceParam, CalculatePriceResult, CalculateResult,
+};
 
 #[tauri::command]
-async fn build_calculate(build_param: BuildParam) -> (Vec<String>, Vec<String>) {
-    println!("{:?}", build_param);
-    calculate::calculate(build_param)
+async fn calculate_build(
+    build_param: CalculatePageParam,
+    artifact_check: bool,
+) -> Result<CalculateResult, ()> {
+    let calculate_result = calculate::calculate_build_fn(build_param, artifact_check).await;
+    Ok(calculate_result)
+}
+
+#[tauri::command]
+async fn calculate_price(
+    param: (Vec<CalculatePriceParam>, Vec<&str>),
+) -> Result<Vec<CalculatePriceResult>, ()> {
+    let calculate_price_result = calculate::calculate_price_fn(param).await;
+    Ok(calculate_price_result)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![build_calculate])
+        .invoke_handler(tauri::generate_handler![calculate_build, calculate_price])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
