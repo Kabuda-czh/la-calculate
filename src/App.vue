aa47f62b98238b06ee7ccc4fc10e5576378026a8
Kabuda-czh <634469564@qq.com>
2024-06-14 17:45:33 +0800

    ✨ feat: add svg title

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,9 @@ onMounted(() => {
       <n-space vertical size="large">
         <n-layout class="h-full">
           <n-layout-header class="flex-c">
-            <p>欢迎使用 La-Calculate</p>
+            <svg viewBox="400 0 400 200">
+              <text x="50%" y="70%" width="100%" height="100%"> La-Calculate </text>
+            </svg>
           </n-layout-header>
           <n-layout-content class="h-full" content-style="padding: 24px;">
             <Calculate />
@@ -53,4 +55,43 @@ onMounted(() => {
   </NConfigProvider>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+svg {
+  width: 100%;
+  height: 100px;
+  margin: auto;
+}
+svg text {
+  text-transform: uppercase;
+  animation: stroke 5s infinite alternate;
+  letter-spacing: 10px;
+  font-size: 90px;
+}
+@keyframes stroke {
+  0% {
+    fill: rgba(72, 138, 20, 0);
+    stroke: rgba(54, 95, 160, 1);
+    stroke-dashoffset: 25%;
+    stroke-dasharray: 0 50%;
+    stroke-width: 0.8;
+  }
+  50% {
+    fill: rgba(72, 138, 20, 0);
+    stroke: rgba(54, 95, 160, 1);
+    stroke-width: 1.2;
+  }
+  70% {
+    fill: rgba(72, 138, 20, 0);
+    stroke: rgba(54, 95, 160, 1);
+    stroke-width: 1.5;
+  }
+  90%,
+  100% {
+    fill: rgba(72, 138, 204, 1);
+    stroke: rgba(54, 95, 160, 0);
+    stroke-dashoffset: -25%;
+    stroke-dasharray: 50% 0;
+    stroke-width: 0;
+  }
+}
+</style>
