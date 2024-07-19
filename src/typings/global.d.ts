c6845a524e2d9c6af8ee52379abc05067fda6088
Kabuda-czh <634469564@qq.com>
2024-07-19 15:32:41 +0800

    🦄 refactor: type string to lower

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -58,7 +58,7 @@ namespace Calculate {
 
   interface CalculatePriceParam {
     accessory_name: string
-    accessory: string & 'Amulet' | 'Earring' | 'Ring'
+    accessory: string & 'amulet' | 'earring' | 'ring'
     build_string: string
     build: Record<string, number>
     is_artifact: 0 | 1
